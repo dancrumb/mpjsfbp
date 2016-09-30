7f035bf5c2ff8cf02089df3146f0b2cc68f6456d
Dan Rumney <drumney@jellyvision.com>
2016-09-30 13:13:31 -0600

    Removed redundant Runtime stuff

--- test/core/InputPort.js
+++ test/core/InputPort.js
@@ -2,53 +2,17 @@
  * Created by danrumney on 5/27/16.
  */
 
-var InputPort = require('../../core/InputPort');
-var IIPConnection = require('../../core/IIPConnection');
-var ProcessConnection = require('../../core/ProcessConnection');
-var IP = require('../../core/IP');
-var FiberRuntime = require('../../core/runtimes/FiberRuntime');
+//var InputPort = require('../../core/InputPort');
 
-
-var getTestInPort = function (connection) {
-  var fiberRuntime = new FiberRuntime();
-  var inPort = new InputPort();
-  inPort.name = "Test InPort";
-  connection.setRuntime(fiberRuntime);
-  inPort.conn = connection;
-  return inPort;
-};
 
 describe.skip('InputPort', function() {
 
 
-  it('can receive IIPs', function(done) {
-    var inPort = getTestInPort(new IIPConnection("test"));
+  it('can receive IIPs', function() {
 
-    global.tracing= true;
-
-    TestFiber(function(mockProcess) {
-      var ip = inPort.receive();
-
-      expect(ip.contents).to.be.equal('test');
-      expect(ip.owner).to.be.equal(mockProcess);
-      expect(ip.type).to.be.equal(IP.NORMAL);
-
-      done();
-    });
   });
 
-  it('returns null from a closed connection', function(done) {
-    var inPort = getTestInPort(new ProcessConnection(1));
-    inPort.conn.closed = true;
+  it('returns null from a closed connection', function() {
 
-    global.tracing= true;
-
-    TestFiber(function() {
-      var ip = inPort.receive();
-
-      expect(ip).to.be.null;
-
-      done();
-    });
   })
 });
--- test/core/runtimes/fiber-runtime.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-describe('FiberRuntime', function () {
-  it.skip('should be able to run a simple sender-->receiver setup', function () {
-
-  });
-});
