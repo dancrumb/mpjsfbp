e1929354294d42bf93323cde4946efc2dd6c88bc
Dan Rumney <drumney@jellyvision.com>
2016-10-16 08:46:28 -0500

    Ensure npm v3 is being used

---
+++ test/core/FBPProcess.js
@@ -0,0 +1,16 @@
+/**
+ * Created by danrumney on 5/27/16.
+ */
+
+import FBPProcess  from '../../src/core/FBPProcess';
+
+
+describe('FBPProcess', function() {
+
+  it('', (done) => {
+    var proc = new FBPProcess({name: "TEST"});
+    done();
+  });
+
+
+});
