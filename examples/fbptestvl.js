cb72920454ab1b25e52a8e205ddc09f6244b45f6
Dan Rumney <drumney@jellyvision.com>
2016-10-16 08:18:34 -0500

    Making sure the examples work

--- examples/fbptestvl.js
+++ examples/fbptestvl.js
@@ -1,11 +1,11 @@
-var fbp = require('../src');
+import fbp from '../src';
 
 // --- define network ---
-var network = new fbp.Network({ componentRoot: __dirname});
+const network = new fbp.Network({ componentRoot: __dirname});
 
-var gendata = network.defProc('/components/gendata.js', 'Gen');
-var copier = network.defProc('./components/copier.js', 'Copy');
-var disc = network.defProc('./components/discard.js', 'Disc');
+const gendata = network.defProc('/components/gendata.js', 'Gen');
+const copier = network.defProc('./components/copier.js', 'Copy');
+const disc = network.defProc('./components/discard.js', 'Disc');
 // var recvr = fbp.defProc(require('../components/recvr.js'), 'recvr'); // equivalent
 
 network.initialize(gendata, 'COUNT', '100000000');
